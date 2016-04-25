6fe6b8f8d8dae82303ef5f810a2dcf8b1c3e005e
Taegyu Ho <dio_g@naver.com>
2016-04-25 16:44:24 +0900

    add 기능 추가

---
+++ src/main/java/seNex/Calculator.java
@@ -0,0 +1,12 @@
+package seNex;
+
+public class Calculator {
+	private int result;
+	public void add(int x, int y){
+		result = x+y;
+	}
+	public int getResult(){
+		return result;
+	}
+
+}
---
+++ src/test/java/seNex/CalculatorTest.java
@@ -0,0 +1,16 @@
+package seNex;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		Calculator cal = new Calculator();
+		cal.add(10,20);
+		assertEquals(cal.getResult(),30);
+	}
+
+}
